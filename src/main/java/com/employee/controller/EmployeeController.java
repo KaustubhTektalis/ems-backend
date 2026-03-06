09df5237666545da9d315344c137d7ad29d4c207
KaustubhJL <kaustubh.j@tektalis.com>
2026-03-06 16:00:59 +0530

    Added services

--- src/main/java/com/employee/controller/EmployeeController.java
+++ src/main/java/com/employee/controller/EmployeeController.java
@@ -1,7 +1,10 @@
 package com.employee.controller;
 
-import java.util.List;
+import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,16 +30,26 @@ public class EmployeeController {
     }
 
     @GetMapping("/employees")
-    public List<EmployeeResponseDTO> getAllEmployees() {
-
-        return employeeService.getAllEmployees();
+    public Page<EmployeeResponseDTO> getAllEmployees(Pageable pageable) {
+        return employeeService.getAllEmployees(pageable);
     }
 
-    @GetMapping("/employee/{empId}")
-    public EmployeeResponseDTO getEmployeeById(
-            @PathVariable String empId) {
-
+    @GetMapping("/id/{empId}")
+    public EmployeeResponseDTO getEmployeeById(@PathVariable String empId) {
         return employeeService.getEmployeeById(empId);
+    }
+    
+    @GetMapping("/name/{name}")
+    public Page<EmployeeResponseDTO> getEmployeesByName(@PathVariable String name, Pageable pageable) {
+        return employeeService.getEmployeesByName(name, pageable);
+    }
+    @GetMapping("/department/{department}")
+    public Page<EmployeeResponseDTO> getEmployeesByDepartment(@PathVariable String department, Pageable pageable) {
+        return employeeService.getEmployeesByDepartment(department, pageable);
+    }
+    @GetMapping("/date/{date}")
+    public Page<EmployeeResponseDTO> getEmployeesByJoiningDate(@PathVariable LocalDate date, Pageable pageable) {
+        return employeeService.getEmployeesByJoiningDate(date, pageable);
     }
 
     @DeleteMapping("/employee/{empId}")
--- src/main/java/com/employee/service/EmployeeService.java
+++ src/main/java/com/employee/service/EmployeeService.java
@@ -1,18 +1,136 @@
 package com.employee.service;
 
-import java.util.List;
-
+import java.time.LocalDate;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import com.employee.dto.CreateEmployeeRequestDTO;
 import com.employee.dto.EmployeeResponseDTO;
+import com.employee.entity.Employee;
+import com.employee.entity.Password;
+import com.employee.entity.Roles;
+import com.employee.entity.SetCompKey;
+import com.employee.entity.UserRoles;
+import com.employee.repository.EmployeeRepository;
+import com.employee.repository.PasswordRepository;
+import com.employee.repository.RolesRepository;
 
-public interface EmployeeService {
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
 
-    EmployeeResponseDTO createEmployee(CreateEmployeeRequestDTO request);
+@Service
+@RequiredArgsConstructor
+public class EmployeeService {
 
-    List<EmployeeResponseDTO> getAllEmployees();
+    private final EmployeeRepository employeeRepository;
+    private final RolesRepository rolesRepository;
+    private final PasswordRepository passwordRepository;
+    
+    @PersistenceContext
+    private EntityManager entityManager;
 
-    EmployeeResponseDTO getEmployeeById(String empId);
+    @Transactional
+    public EmployeeResponseDTO createEmployee(CreateEmployeeRequestDTO dto) {
 
-    void deleteEmployee(String empId);
+    	Employee employee = Employee.builder()
+                .name(dto.getName())
+                .companyEmail(dto.getCompanyEmail())
+                .personalEmail(dto.getPersonalEmail())
+                .phoneNumber(dto.getPhoneNumber())
+                .address(dto.getAddress())
+                .department(dto.getDepartment())
+                .designation(dto.getDesignation())
+                .dateOfJoin(dto.getDateOfJoin())
+                .dateOfBirth(dto.getDateOfBirth())
+                .description(dto.getDescription())
+                .build();
 
+    	Employee savedEmployee = employeeRepository.save(employee);
+
+
+        Password password = Password.builder()
+                .employee(savedEmployee)
+                .password("pass")
+                .build();
+
+        passwordRepository.save(password);
+
+        Set<UserRoles> userRoles = dto.getRoles().stream()
+                .map(roleEnum -> {
+
+                    Roles role = rolesRepository.findByRole(roleEnum);
+
+                    return UserRoles.builder()
+                            .employee(savedEmployee)
+                            .role(role)
+                            .comp(new SetCompKey(savedEmployee.getEmpId(), role.getRoleId()))
+                            .build();
+                })
+                .collect(Collectors.toSet());
+
+        employee.setRoles(userRoles);
+
+        return buildResponse(savedEmployee);
+    }
+
+
+
+    public Page<EmployeeResponseDTO> getAllEmployees(Pageable pageable) {
+        return employeeRepository.findByIsEmployeeActiveTrue(pageable);
+    }
+    
+
+    public Page<EmployeeResponseDTO> getEmployeesByName(String name, Pageable pageable) {
+        return employeeRepository.findByNameContainingIgnoreCaseAndIsEmployeeActiveTrue(name, pageable);
+    }
+     
+
+    public Page<EmployeeResponseDTO> getEmployeesByDepartment(String department, Pageable pageable) {
+        return employeeRepository.findByDepartmentIgnoreCaseAndIsEmployeeActiveTrue(department, pageable);
+    }
+     
+
+    public Page<EmployeeResponseDTO> getEmployeesByJoiningDate(LocalDate date, Pageable pageable) {
+        return employeeRepository.findByDateOfJoinGreaterThanEqualAndIsEmployeeActiveTrue(date, pageable);
+    }
+   
+
+    public EmployeeResponseDTO getEmployeeById(String empId) {
+        Employee employee = employeeRepository.findById(empId)
+                .orElseThrow(() -> new EntityNotFoundException("Employee not found with id: " + empId));
+     
+        if (!Boolean.TRUE.equals(employee.getIsEmployeeActive())) {
+            throw new EntityNotFoundException("Employee is not active");
+        }
+        return buildResponse(employee);
+    }
+    
+
+
+    public void deleteEmployee(String empId) {
+
+    	Employee employee = employeeRepository.findById(empId)
+				.orElseThrow(() -> new EntityNotFoundException("Employee with id " + empId + " not found"));
+
+    	employee.setIsEmployeeActive(false);
+		employee.setDateOfExit(LocalDate.now());
+		employeeRepository.save(employee);
+    }
+    
+    
+    private EmployeeResponseDTO buildResponse(Employee employee) {
+        return EmployeeResponseDTO.builder()
+        		.empId(employee.getEmpId())
+                .name(employee.getName())
+                .companyEmail(employee.getCompanyEmail())
+                .department(employee.getDepartment())
+                .designation(employee.getDesignation())
+                .dateOfJoin(employee.getDateOfJoin())
+                .build();
+    }
 }
